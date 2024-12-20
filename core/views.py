12f330859ee0f9da62ca956b12e06b7cde1746a2
Joshua Pereira <71769388+Josh7511@users.noreply.github.com>
2024-12-20 04:47:52 -0500

    login page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
+def loginPage(request):
+    context ={}
+    return render(request, "loginPage.html", context)
 
 def home(request):
     return render(request, "frontPage.html" )
